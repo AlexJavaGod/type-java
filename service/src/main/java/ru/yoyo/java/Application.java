162d3f637280d904dd76eeb3aef6240a968ac3f0
Electroman <s@mail.ru>
2022-12-24 20:07:26 +0300

    Типы данных.

---
+++ service/src/main/java/ru/yoyo/java/Application.java
@@ -0,0 +1,46 @@
+package ru.yoyo.java;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+//        Сумма двух чисел
+//        int number = 2;
+//        int number2 = 3;
+//        int summa = number + number2;
+//        System.out.println(summa);
+//        Минусовка чисел
+//        int number = 101;
+//        int number2 = 100;
+//        int minus = number - number2;
+//        System.out.println(minus);
+//        Умножение
+//        int number = 101;
+//        int number2 = 100;
+//        int multiply = number * number2;
+//        System.out.println(multiply);
+//        Целочисленное деление
+//        int number = 8;
+//        int number2 = 3;
+//        int div = number / number2;
+//        System.out.println(div);
+//ОСТАТОК ОТ ДЕЛЕНИЯ ЧИСЛЕННЫЙ
+//        int number = 2195;
+//        int number2 = 5;
+//        int ostatok = number % number2;
+//        System.out.println(ostatok);
+//        Вещественное деление с точкой
+//
+//        double number = 36756;
+//        double number2 = 8568;
+//        double div = number / number2;
+//        System.out.println(div);
+
+        double number3 = 4.2899159663865545;
+        double number4 = 8568;
+        double multiply = number3 * number4;
+        System.out.println(multiply);
+
+    }
+
+}
